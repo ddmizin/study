1122b8450e93064d09dd45bb66ab66e27b58676f
ddmizin <ddmizin@mail.ru>
2026-01-29 00:25:26 +0300

    29.01.26

---
+++ classes/classes1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstring>
+
+struct S {
+private:
+    int l = 2;
+    friend void g(S, int);
+public:
+    int x = 1;
+    void f(int y) const {
+        std::cout << x + y << '\n';
+    }
+    void ff() const {
+        std::cout << this->x << '\n';
+    }
+    struct C {
+        char c = 'a';
+        char& k(){
+            return c;
+        }
+    };
+};
+
+void g(S s, int y){
+    std::cout << s.l + y << '\n';
+}
+
+
+class Complex {
+public:
+    double re = 0.0;
+    double im = 0.0;
+public:
+    Complex() = default;
+    Complex(double re);
+    Complex(double re, double im): re(re), im(im) {}
+    Complex(const Complex& other) = default;
+};
+
+Complex::Complex(double re): re(re) {}
+
+
+class String {
+public:
+    char* arr;
+    size_t sz;
+    size_t cap;
+public:
+    String() {} // default constructor
+    String(size_t n, char c): arr(new char[n + 1]), sz(n), cap(n + 1) {
+        memset(arr, c, n);
+        arr[sz] = '\0';
+    }
+    String(std::initializer_list<char> list)
+            : arr(new char[list.size() + 1]), sz(list.size()), cap(sz + 1) {
+        std::copy(list.begin(), list.end(), arr);
+        arr[sz] = '\0';
+    }
+    String(const String& other): arr(new char[other.cap]), sz(other.sz), cap(other.cap) { // copy constructor
+        memcpy(arr, other.arr, sz + 1);
+    }
+    ~String() {
+        delete[] arr;
+    }
+};
+
+int main(){
+    S s;
+    std::cout << s.x << '\n';
+    s.f(5);
+    s.ff();
+    S::C c;
+    std::cout << sizeof(s) << '\n';
+    std::cout << c.k() << '\n';
+    g(s, 5);
+
+    Complex cmp = {7.0};
+    Complex cmp2 = {5.4, 6.7};
+    Complex cmp3;
+    Complex cmp4 = cmp2;
+    std::cout << cmp4.im;
+
+    String str; // default initialization
+    String str2(5, 'r');
+    String str3 = {'a', 'b', 'c', 'd'};
+    String str4 = str2;
+}
---
+++ classes/classes2.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+
+class String {
+public:
+    char* arr;
+    size_t sz;
+    size_t cap;
+
+    String(size_t n): arr(new char[n + 1]), sz(n), cap(n + 1) { // delegating constructor
+        arr[sz] = '\0';
+    }
+public:
+    String() = default;
+    String(int n, char c): String(n) {
+        memset(arr, c, n);
+    }
+    String(std::initializer_list<char> list): String(list.size()) {
+        std::copy(list.begin(), list.end(), arr);
+    }
+    String(const String& other): String(other.sz) {
+        memcpy(arr, other.arr, sz);
+    }
+
+    String& operator=(String other) { // Copy-and-Swap idiom
+        swap(other);
+        return *this;
+    }
+
+    ~String() {
+        delete[] arr;
+    }
+
+    void swap(String& other) {
+        std::swap(arr, other.arr);
+        std::swap(sz, other.sz);
+        std::swap(cap, other.cap);
+    }
+};
+
+
+struct S {
+    char arr[10];
+    static int x;
+    static const int y = 0;
+
+    static void g() {
+        std::cout << "Hi!" << '\n';
+    }
+    void f() const {
+        std::cout << 1 << '\n';
+    }
+    void f() {
+        std::cout << 2 << '\n';
+    }
+
+    char& operator[](size_t index){
+        return arr[index];
+    }
+    const char& operator[](size_t index) const{
+        return arr[index];
+    }
+};
+
+int S::x = 1;
+
+
+class Singleton {
+private:
+    static Singleton* ptr;
+
+    Singleton() = default;
+    Singleton(const Singleton& other) = delete;
+
+    Singleton& operator=(const Singleton& other) = delete;
+public:
+    static Singleton& GetObject() {
+        if (ptr == nullptr) {
+            ptr = new Singleton();
+        }
+        return *ptr;
+    }
+};
+
+Singleton* Singleton::ptr = nullptr;
+
+
+struct Latitude {
+    double value;
+
+    explicit Latitude(double value): value(value) {}
+    explicit operator double() const {
+        return value;
+    }
+};
+
+struct Longitude {
+    double value;
+
+    explicit Longitude(double value): value(value) {}
+    explicit operator double() const{
+        return value;
+    }
+};
+
+Latitude operator""_lat(long double x){
+    return Latitude(x);
+}
+
+Longitude operator""_log(long double x){
+    return Longitude(x);
+}
+
+
+class BigInteger {};
+
+BigInteger operator""_bi(unsigned long long x){
+    return BigInteger();
+}
+
+int main() {
+    // String s; // default initialization
+    // String s2(5, 'r');
+    // String s3 = {'a', 'b', 'c', 'd'};
+    // String s4 = s3;
+    // s2 = s2;
+    // std::cout << s2.arr;
+
+    // S::g();
+    S s;
+    // const S& r = s;
+    // s.f();
+    // r.f();
+
+    Singleton& slt = Singleton::GetObject();
+    Latitude l(4.6);
+    Latitude m = static_cast<Latitude>(4.7);
+    double k = static_cast<double>(l);
+
+    BigInteger bi = 1_bi;
+
+    Latitude la = 4.7_lat;
+    Longitude lo = 2.5_log;
+}
