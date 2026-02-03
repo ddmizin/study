a2b9786016e0443f9084bd604d4f3449ca862883
ddmizin <ddmizin@mail.ru>
2026-02-03 13:16:58 +0300

    kg

---
+++ saewfcvq.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <cstring>
+
+template <typename T>
+class Vector {
+    T* arr;
+    size_t sz;
+    size_t cap;
+
+    Vector(size_t sz, size_t cap): arr(new T[cap]). sz(sz), cap(cap) {}
+    
+    
+public:
+    Vector(): arr(nullptr), sz(0), cap(0) {}
+
+    explicit Vector(size_t n): Vector(n, n) {
+        if (n > 0) {
+            std::memset(arr, 0, n * sizeof(T));
+        }
+    }
+
+
+
+};
+
+int main() {}
