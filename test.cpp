a53a9c05747abe7fdcd1128a112e528f4e86d874
ddmizin <ddmizin@mail.ru>
2026-02-11 20:37:06 +0300

    11.02.26

--- test.cpp
+++ test.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <compare>
 
-class Complex2 {
-    double real, imag;
-public:
-    Complex2(double r, double i) : real(r), imag(i) {}
-    
-    // Автоматическая генерация всех операторов сравнения
-    auto operator<=>(const Complex2& other) const = default;
+struct Base {
+    virtual void f(int x) {
+        std::cout << 1 << '\n';
+    }
+};
+
+struct Derived: Base {
+    void f(int x) override{
+        std::cout << 2 << '\n';
+    }
 };
 
 int main() {
-    Complex2 a(1.0, 2.0);
-    Complex2 b(1.0, 2.0);
-    Complex2 c(2.0, 1.0);
-    
-    std::cout << std::boolalpha;
-    std::cout << "a == b: " << (a == b) << std::endl;  // true
-    std::cout << "a != b: " << (a != b) << std::endl;  // false
-    std::cout << "a < c: " << (a < c) << std::endl;    // зависит от порядка членов
-    
-    return 0;
+    int x;
+    Derived d;
+    Base* b = &d;
+    d.f(x);
+    b->f(x);
 }
