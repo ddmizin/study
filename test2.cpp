c50c3de444645840d9c89b8beb5737dff7e205df
ddmizin <ddmizin@mail.ru>
2026-04-01 14:00:43 +0300

    dfn

--- test2.cpp
+++ test2.cpp
@@ -1,28 +1,105 @@
 #include <iostream>
+#include <vector>
 
-const int MAX_R = 100;
+using namespace std;
 
-void sort_by_counting(int n) {
-    int freq[MAX_R + 1];
-    for (int i = 0; i <= MAX_R; ++i) {
-        freq[i] = 0;
-    }
-    for (int i = 0; i < n; ++i) {
-        int r;
-        std::cin >> r;
-        int less = 0;
-        for (int v = 1; v < r; ++v) {
-            less += freq[v];
+
+vector<vector<int>> adjL, adjR;
+vector<int> mt; 
+vector<int> matchL;  
+vector<bool> used;
+
+
+bool try_kuhn(int v) {
+    if (used[v]) return false;
+    used[v] = true;
+    for (int to : adjL[v]) {
+        if (mt[to] == -1 || try_kuhn(mt[to])) {
+            mt[to] = v;
+            matchL[v] = to;
+            return true;
         }
-        int equal = freq[r];
-        int pos = less + equal;
-        std::cout << pos << " ";
-        freq[r] += 1;
+    }
+    return false;
+}
+
+vector<bool> winP_L, winP_R;
+
+
+void dfsL(int u) {
+    winP_L[u] = true;
+    for (int v : adjL[u]) {
+        if (matchL[u] == v) continue;
+        int next_u = mt[v];
+        
+        if (next_u != -1 && !winP_L[next_u]) {
+            dfsL(next_u);
+        }
+    }
+}
+
+
+void dfsR(int v) {
+    winP_R[v] = true; 
+    for (int u : adjR[v]) {
+        if (mt[v] == u) continue; 
+        int next_v = matchL[u];
+        
+        if (next_v != -1 && !winP_R[next_v]) {
+            dfsR(next_v);
+        }
     }
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    sort_by_counting(n);
+    int n1, n2, m;
+    if (!(cin >> n1 >> n2 >> m)) return 0;
+
+    adjL.resize(n1 + 1);
+    adjR.resize(n2 + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adjL[u].push_back(v);
+        adjR[v].push_back(u);
+    }
+
+    mt.assign(n2 + 1, -1);
+    matchL.assign(n1 + 1, -1);
+
+    
+    for (int i = 1; i <= n1; ++i) {
+        used.assign(n1 + 1, false);
+        try_kuhn(i);
+    }
+
+    winP_L.assign(n1 + 1, false);
+    
+    for (int i = 1; i <= n1; ++i) {
+        if (matchL[i] == -1 && !winP_L[i]) {
+            dfsL(i);
+        }
+    }
+
+    winP_R.assign(n2 + 1, false);
+    
+    for (int i = 1; i <= n2; ++i) {
+        if (mt[i] == -1 && !winP_R[i]) {
+            dfsR(i);
+        }
+    }
+
+    
+    for (int i = 1; i <= n1; ++i) {
+        cout << (winP_L[i] ? 'P' : 'N');
+    }
+    cout << "\n";
+
+    
+    for (int i = 1; i <= n2; ++i) {
+        cout << (winP_R[i] ? 'P' : 'N');
+    }
+    cout << "\n";
+
+    return 0;
 }
--- test3.cpp
+++ test3.cpp
@@ -5,33 +5,36 @@
 
 using namespace std;
 
-const long long INF = 1e18;
+const int INF = 1e9;
 
 
 struct Edge {
     int to;
-    long long cap;
-    long long flow;
-    long long cost;
+    int cap;
+    int flow;
+    int cost;
     int rev;
     int id;
 };
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n, m, e;
     if (!(cin >> n >> m >> e)) return 0;
 
-    vector<long long> w_L(n + 1);
+    vector<int> w_L(n + 1);
     for (int i = 1; i <= n; ++i) cin >> w_L[i];
 
-    vector<long long> w_R(m + 1);
+    vector<int> w_R(m + 1);
     for (int i = 1; i <= m; ++i) cin >> w_R[i];
 
     int S = 0;
-    int T = n + m + 1;
+    int T = n + m + 1; 
     vector<vector<Edge>> adj(n + m + 2);
 
-    auto add_edge = [&](int u, int v, long long cap, long long cost, int id) {
+    auto add_edge = [&](int u, int v, int cap, int cost, int id) {
         adj[u].push_back({v, cap, 0, cost, (int)adj[v].size(), id});
         adj[v].push_back({u, 0, 0, -cost, (int)adj[u].size() - 1, 0});
     };
@@ -51,7 +54,7 @@ int main() {
     }
 
     
-    vector<long long> pot(n + m + 2, INF);
+    vector<int> pot(n + m + 2, INF);
     pot[S] = 0;
     
     
@@ -75,14 +78,17 @@ int main() {
 
     long long max_weight = 0;
 
+    vector<int> dist(n + m + 2);
+    vector<int> parent_node(n + m + 2);
+    vector<int> parent_edge(n + m + 2);
     
     while (true) {
-        vector<long long> dist(n + m + 2, INF);
-        vector<int> parent_node(n + m + 2, -1);
-        vector<int> parent_edge(n + m + 2, -1);
+        dist.assign(n + m + 2, INF);
+        parent_node.assign(n + m + 2, -1);
+        parent_edge.assign(n + m + 2, -1);
         
         
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
         dist[S] = 0;
         pq.push({0, S});
@@ -92,11 +98,12 @@ int main() {
             pq.pop();
 
             if (d > dist[u]) continue;
+            if (u == T) break;
 
             for (int i = 0; i < adj[u].size(); ++i) {
                 auto& edge = adj[u][i];
                 if (edge.cap - edge.flow > 0 && pot[u] != INF && pot[edge.to] != INF) {
-                    long long new_cost = dist[u] + edge.cost + pot[u] - pot[edge.to];
+                    int new_cost = dist[u] + edge.cost + pot[u] - pot[edge.to];
                     if (dist[edge.to] > new_cost) {
                         dist[edge.to] = new_cost;
                         parent_node[edge.to] = u;
@@ -111,7 +118,7 @@ int main() {
         
         if (dist[T] + pot[T] >= 0) break;
 
-        long long true_cost = dist[T] + pot[T];
+        int true_cost = dist[T] + pot[T];
         max_weight += -true_cost;
 
         int curr = T;
@@ -126,8 +133,8 @@ int main() {
         }
 
         for (int i = 0; i <= n + m + 1; ++i) {
-            if (pot[i] != INF && dist[i] != INF) {
-                pot[i] += dist[i];
+            if (pot[i] != INF) {
+                pot[i] += min(dist[i], dist[T]);
             }
         }
     }
